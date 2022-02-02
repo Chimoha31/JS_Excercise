6b5797b17c0ac3abfa990d693e7f308b75eeb9c8
Chimoha31 <mokochii1108@gmail.com>
2022-02-02 00:22:41 -0800

    DONE EXCEPT 45

--- main.js
+++ main.js
@@ -1,2 +1,51 @@
-'use strict';
+"use strict";
 
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const ul = document.querySelector("#ul");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+todos.forEach((todo) => {
+  add(todo);
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  add();
+});
+
+function add(todo) {
+  let todoText = input.value;
+  if (todo) {
+    todoText = todo;
+  }
+
+  if (todoText.length > 0) {
+    let li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    console.log(ul);
+    input.value = "";
+
+    save();
+  }
+}
+
+function save() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach((list) => {
+    todos.push(list.innerText);
+    console.log(todos);
+
+    list.addEventListener("click", () => {
+      for (let i = 0; i < todos.length; i++) {
+        list.classList.toggle("done");
+      }
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
