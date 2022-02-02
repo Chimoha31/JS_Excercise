2c127808b17b597b18474f1ee6f179952d38acfb
Chimoha31 <mokochii1108@gmail.com>
2022-02-02 14:55:21 -0800

    DONE exercise 3

--- exercise.js
+++ exercise.js
@@ -19,8 +19,24 @@ half("I am a student");
 const concatenate = (first, second) => {
   // console.log(first.slice(1, first.length) + second.slice(1, second.length));
   console.log(first.slice(1) + second.slice(1));
-}
+};
 
 concatenate("abc", "def");
 concatenate("Japanese", "Canadian");
 
+// ------------------------------------------------------------
+// ③Given two value, write a JS program to find out which one is nearest to 100
+const closestNum = (num1, num2) => {
+  const numA = 100 - num1;
+  const numB = 100 - num2;
+
+  if (numA < numB) {
+    return `${num1} is closest to 100 than ${num2}`;
+  } else if (numA > numB) {
+    return `${num2} is closest to 100 than ${num1}`;
+  }else{
+    return `${num1} & ${num2} is same number. Can't compare`
+  }
+};
+
+console.log(closestNum(79, 80));
