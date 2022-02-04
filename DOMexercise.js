4cf5f710d76d57623079c0416b49425a9d816b2d
Chimoha31 <mokochii1108@gmail.com>
2022-02-03 17:43:56 -0800

    DONE 2

--- DOMexercise.js
+++ DOMexercise.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /*
 Exercises 01
 ------------
@@ -7,19 +6,30 @@ Add a label to each of the input fields: username, password, confirm password
 */
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
-const confirmPSW = document.querySelector('#confirmPassword');
+const confirmPWD = document.querySelector('#confirmPassword');
+const div = document.querySelectorAll('.col');
 
 username.insertAdjacentHTML('beforeBegin', '<label for="username">Username: </label>');
-password.insertAdjacentHTML('beforeBegin', '<label for="password">Passwoord: </label>')
-confirmPSW.insertAdjacentHTML('beforeBegin', '<label foe="confirmPassword">Confirm Password: </label>')
-
+password.insertAdjacentHTML('beforeBegin', '<label for="password">Passwoord: </label>');
+confirmPWD.insertAdjacentHTML('beforeBegin', '<label foe="confirmPassword">Confirm Password: </label>');
 
 /*
   Exercise 02
   -----------
   Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
 */
-
+const checkInput = (e) => {
+  const errorEl = Array.from(e.target.parentNode.querySelectorAll('span'));
+  if(e.target.value === "" && !errorEl.length) {
+    e.target.insertAdjacentHTML('afterEND', '<span class="text-danger">Required</span>');
+  }
+  if(errorEl && e.target.value !== '') {
+    errorEl.forEach(el => el.remove());
+  }
+};
+username.addEventListener('blur', checkInput);
+password.addEventListener('blur', checkInput);
+confirmPWD.addEventListener('blur', checkInput);
 
 /*
   Exercise 03
